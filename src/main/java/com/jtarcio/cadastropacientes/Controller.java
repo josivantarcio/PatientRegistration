89c1d5a97477afe884f8435f400783ba2ac047da
Josivan Tarcio <josivantarcio@gmail.com>
2025-11-14 01:38:54 -0300

    controller create

---
+++ src/main/java/com/jtarcio/cadastropacientes/Controller.java
@@ -0,0 +1,15 @@
+package com.jtarcio.cadastropacientes;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Essa é a minha primeira mensagem nessa rota";
+    }
+}
