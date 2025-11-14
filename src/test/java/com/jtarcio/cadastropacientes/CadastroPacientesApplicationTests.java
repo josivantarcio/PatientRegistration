a735b62fb956f564a921cc361d215ec0e6b8e085
Josivan Tarcio <josivantarcio@gmail.com>
2025-11-14 00:42:52 -0300

    primeiro commit

---
+++ src/test/java/com/jtarcio/cadastropacientes/CadastroPacientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtarcio.cadastropacientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroPacientesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
