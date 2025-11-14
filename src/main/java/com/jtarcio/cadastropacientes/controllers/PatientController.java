37f1f290e902bb41ad9d63392c42661a03e337f2
Josivan Tarcio <josivantarcio@gmail.com>
2025-11-14 09:52:25 -0300

    criar a entidade tb_pacientes no banco

--- src/main/java/com/jtarcio/cadastropacientes/controllers/PatientController.java
+++ src/main/java/com/jtarcio/cadastropacientes/controllers/PatientController.java
@@ -1,4 +1,4 @@
-package com.jtarcio.cadastropacientes;
+package com.jtarcio.cadastropacientes.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class Controller {
+public class PatientController {
 
     @GetMapping("/boasvindas")
     public String boasVindas(){
---
+++ src/main/java/com/jtarcio/cadastropacientes/entities/PatientEntity.java
@@ -0,0 +1,86 @@
+package com.jtarcio.cadastropacientes.entities;
+
+import jakarta.persistence.*;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.cglib.core.GeneratorStrategy;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_pacientes")
+public class PatientEntity implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Integer age;
+    private String email;
+
+    public PatientEntity() {
+    }
+
+    public PatientEntity(String name, Integer age, String email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        PatientEntity that = (PatientEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "PatientEntity{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
